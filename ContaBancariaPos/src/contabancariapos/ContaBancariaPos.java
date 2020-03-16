5c58dba91e61a718399311b55d4bb67ce44630e7
Pedro Neto <pedro@tuxcuiabano.com>
2020-03-16 19:21:41 -0400

    olá mundo

--- ContaBancariaPos/src/contabancariapos/ContaBancariaPos.java
+++ ContaBancariaPos/src/contabancariapos/ContaBancariaPos.java
@@ -16,6 +16,8 @@ public class ContaBancariaPos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Olá, mundo");
+        
     }
     
 }
